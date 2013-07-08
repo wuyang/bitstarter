4779e4c179657a6fd78d96f7ddfab2ac573ae431
Yang Wu <wuyang80@gmail.com>
2013-07-08 14:01:55 +0000

    read from file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var infile = "index.html";
+var buf = fs.readFileSync(infile);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
